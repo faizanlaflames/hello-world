a70018634489d6ec6d1d970cc6b0a0f571196dbc
faizanlaflames <57748609+faizanlaflames@users.noreply.github.com>
2021-02-27 21:05:42 -0700

    Swapping Code to Intended Functions
    Moved around the code for findFactorial() and reverseNumber() in order for them to work properly.

--- GitDemo.java
+++ GitDemo.java
@@ -22,24 +22,7 @@ public class GitDemo {
      * i.e. if num = 4,
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
-    public double findFactorial(int num) {
-        double factorialNumber = 0;
-        
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
-        }
-        
-        return factorialNumber;
-    }
-    
-    /*****
-     * This function should reverse 'num' and return the resulting value.
-     * i.e. if num = 1234
-     *  the function will return 4321
-     *****/
-    public double reverseNumber(int num){
+    public double findFactorial(double num) {
         double newNumber = 1;
         
         for(int i = 1; i <= num; ++i){
@@ -48,5 +31,22 @@ public class GitDemo {
         
         return newNumber;
     }
-    
+    /*****
+     * This function should reverse 'num' and return the resulting value.
+     * i.e. if num = 1234
+     *  the function will return 4321
+     *****/
+    public double reverseNumber(int num){
+        int factorialNumber = 0;
+
+       while (num != 0) {
+           int temp = num % 10;
+           factorialNumber = factorialNumber * 10 + temp;
+           num /= 10;
+       }
+
+       return factorialNumber;
+
+   }
+   
 }
